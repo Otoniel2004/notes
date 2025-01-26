6bce8c822e39e3b7e8ae8cedd24be2a0e73e209d
Otoniel Rocha <oton.rocha84@gmail.com>
2025-01-25 22:43:03 -0300

    LayoutAndAuthentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view("login");
+    }
+
+    public function loginSubmit(Request $request){
+        echo $request->input("text_username");
+        echo "<br>";
+        echo $request->input("text_password");
+    }
+
+    public function logout(){
+        echo "Logout";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+
+// auth routes
+
+Route::get("/login", [AuthController::class, "login"]);
+
+Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
+
+Route::get("/logout", [AuthController::class, "logout"]);
